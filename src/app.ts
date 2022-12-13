936ff7f300c8e4d64f0f456698e1cba232adb1e3
Gyozo Miklos <nyosdroid@gmail.com>
2022-12-13 21:58:52 +0100

    done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from "./hero"; './hero';
+import { TransformerHero } from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+  new HumanHero(1, 'Rincewind', 'M', 40, 50),
+  new HumanHero(2, 'Twoflowers', 'M', 44, 60),
+  new HumanHero(3, 'Conina', 'F', 19, 90)
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  new TransformerHero(1, 'Luggage', 0, 80, 'Twoflowers'),
+  new TransformerHero(2, 'Airplane', 2, 6, 'neutral'),
+  new TransformerHero(3, 'Dragon', 2, 0, 'neutral')
+];
